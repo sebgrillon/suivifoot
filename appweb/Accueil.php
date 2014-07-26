bf10f2e88dd32e61b6d1b28857b564baab2c5a02
bas045@gmail.com <bas045@gmail.com@6e3bef2a-248e-abef-9540-c75b4b25f078>
2014-07-26 13:58:03 +0000

    correction chemin

--- appweb/Accueil.php
+++ appweb/Accueil.php
@@ -1,9 +1,9 @@
 <?php
 //On inclut pour chaque page, l'entete générale au site (html, css, connexion bdd, ...)
-include 'appweb/Entete.php';
+include 'Entete.php';
 
 
 
 //On inclut pour chaque page, le bas de page générale au site (html, css, connexion bdd, ...)
-include 'appweb/PiedPage.php';
+include 'PiedPage.php';
  ?>
